9af7155495f0e40932f0646e30b07f3a2c321cf0
2011140089h <2011140089h@ed.fuk.kindai.ac.jp>
2022-11-16 08:47:28 +0000

    Revert "add model:User-Bookshelf_Link"
    This reverts commit e24c27e5d09498a85af23bd8bde1e9e608b8a2a5.

--- app/models/user_book_shelf_link.rb
+++
@@ -1,2 +0,0 @@
-class UserBookShelfLink < ApplicationRecord
-end
--- test/models/user_book_shelf_link_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserBookShelfLinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
