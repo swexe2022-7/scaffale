2e94ee14800bfa95d6257d9f5f70b742022003b0
2011140089h <2011140089h@ed.fuk.kindai.ac.jp>
2022-11-23 10:47:23 +0000

    add api verification code

--- app/controllers/librarian_controller.rb
+++ app/controllers/librarian_controller.rb
@@ -1,2 +1,27 @@
+require "net/http"
+require "uri"
+require "json"
+
 class LibrarianController < ApplicationController
+    def index
+        #nanikakaku
+    end
+    
+    
+    
+    def verification
+        keyword="ハリーポッター"
+        
+        json=googlebookapi_req(keyword)
+        @data=JSON.parse(json)
+        
+    end
+    
+    def googlebookapi_req(search_str)
+        apiurl="https://www.googleapis.com/books/v1/volumes?q="
+        
+        urlpath=apiurl+URI.encode_www_form_component(search_str)
+        url=URI.parse(urlpath)
+        return Net::HTTP.get(url)
+    end
 end
