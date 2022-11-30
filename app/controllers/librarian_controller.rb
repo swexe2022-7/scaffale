898c88a993159cfbc100275e7c1ec002b9f669ef
2011140089h <2011140089h@ed.fuk.kindai.ac.jp>
2022-11-30 07:29:50 +0000

    add verificaton code

--- app/controllers/librarian_controller.rb
+++ app/controllers/librarian_controller.rb
@@ -11,9 +11,9 @@ class LibrarianController < ApplicationController
     
     def verification
         keyword="ハリーポッター"
+        @data=googlebookapi_data(keyword)
+        #@data=JSON.parse(googlebookapi_req(keyword))['items']
         
-        json=googlebookapi_req(keyword)
-        @data=JSON.parse(json)
         
     end
     
@@ -23,5 +23,23 @@ class LibrarianController < ApplicationController
         urlpath=apiurl+URI.encode_www_form_component(search_str)
         url=URI.parse(urlpath)
         return Net::HTTP.get(url)
+    end
+    
+    def googlebookapi_data(search_str)
+        @booksdata=[]
+        bookinfo=Struct.new(:id, :title, :thumbnail, :link)
+        data=JSON.parse(googlebookapi_req(search_str))['items']
+        
+        data.each do |d|
+            id=d["id"]
+            d=d["volumeInfo"]
+            title=d["title"]
+            thumbnail=d["thumbnail"]
+            link=d["infoLink"]
+            b=bookinfo.new(id,title,thumbnail,link)
+            @booksdata.push(b)
+        end
+        
+        return @booksdata
     end
 end
